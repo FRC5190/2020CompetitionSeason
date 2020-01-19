82c3feb413904e62c7605e540663995e96819622
Prateek Machiraju <prateek.machiraju@gmail.com>
2020-01-19 18:39:53 -0500

    Formatting and clean-up

--- src/main/kotlin/org/ghrobotics/frc2020/auto/routines/TrajectoryRoutine.kt
+++ src/main/kotlin/org/ghrobotics/frc2020/auto/routines/TrajectoryRoutine.kt
@@ -1,3 +1,11 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright 2019, Green Hope Falcons
+ */
+
 package org.ghrobotics.frc2020.auto.routines
 
 import edu.wpi.first.wpilibj2.command.Command
@@ -7,14 +15,14 @@ import org.ghrobotics.frc2020.auto.Paths
 import org.ghrobotics.frc2020.subsystems.Superstructure
 import org.ghrobotics.frc2020.subsystems.drivetrain.Drivetrain
 import org.ghrobotics.lib.commands.parallel
+import org.ghrobotics.lib.commands.parallelDeadline
 import org.ghrobotics.lib.commands.sequential
 
 class TrajectoryRoutine : AutoRoutine {
     override fun getRoutine(): Command {
         return sequential {
-            +parallel {
-                +InstantCommand(Runnable { Drivetrain.resetPosition(Paths.frontOfGoalToTrench.initialPose) })
-                +Drivetrain.followTrajectory(Paths.frontOfGoalToTrench)
+            +InstantCommand(Runnable { Drivetrain.resetPosition(Paths.frontOfGoalToTrench.initialPose) })
+            +parallelDeadline(Drivetrain.followTrajectory(Paths.frontOfGoalToTrench)) {
                 +Superstructure.aimTurret()
             }
             +Superstructure.aimTurret()
