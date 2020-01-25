58d9276674bbfe6e2b80876d0b1cd5a914225e74
Goobwabber <47616186+Goobwabber@users.noreply.github.com>
2020-01-25 15:26:02 -0500

    Add manual command

--- src/main/kotlin/org/ghrobotics/frc2020/subsystems/feeder/Feeder.kt
+++ src/main/kotlin/org/ghrobotics/frc2020/subsystems/feeder/Feeder.kt
@@ -1,9 +1,16 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright 2019, Green Hope Falcons
+ */
+
 package org.ghrobotics.frc2020.subsystems.feeder
 
 import com.revrobotics.CANSparkMaxLowLevel
 import org.ghrobotics.frc2020.FeederConstants
 import org.ghrobotics.lib.commands.FalconSubsystem
-import org.ghrobotics.lib.mathematics.units.nativeunit.DefaultNativeUnitModel
 import org.ghrobotics.lib.motors.rev.FalconMAX
 
 object Feeder : FalconSubsystem() {
@@ -16,6 +23,10 @@ object Feeder : FalconSubsystem() {
 
     // Create PeriodicIO
     private val periodicIO = PeriodicIO()
+
+    fun setPercent(percent: Double) {
+        periodicIO.output = Output.Percent(percent)
+    }
 
     override fun periodic() {
         when (val output = periodicIO.output) {
